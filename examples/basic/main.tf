82612eb02ff6e94838dd1ab982864e6c3fe55a0a
Dylan Vanmali <dylanvanmali@yahoo.com>
2025-02-03 09:14:18 -0800

    feat: tag 1.2.2

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -37,7 +37,7 @@ provider "google" {
 
 module "gke-surrealdb" {
   source = "dvanmali/surrealdb/google"
-  version = "1.2.1"
+  version = "1.2.2"
 
   project_id = local.project_id
   vpc = local.vpc
